4b7fdb9f71cdf9453412465a6acfa9ce619037e6
Sergey Kudelin <kudelin.work@bk.ru>
2022-08-23 14:27:46 +0300

    Update: TDD tested

--- squareTDD.php
+++ squareTDD.php
@@ -1,22 +1,33 @@
 <?php
-    include 'square1.php';
+    include 'square.php';
 
     use PHPUnit\Framework\TestCase;
 
-    class NumSquareTestTDD extends TestCase {
+    class emptyClass {
+
+    }
+
+    
+
+    class squareTDD extends TestCase {
+        /**
+         * @return array
+         */
         public function testNumSquareDataProvider() {
+            $testObject = new emptyClass;
+            $testArr = [];
             return [
-                [2, 4],
-                [-4, 16],
-                [10, 100],
-                [0.2, 0.04],
-                [-0.4, 0.016],
-                [0.01, 0.0001],
-                ['str', 'str'],
-                ['2', '2'],
-                ['@#$%', '@#$%'],
-                [$testObject, $testObject],
-                [$testArr[], $testArr[]], 
+                "num1" => [2, 4],
+                "num2" => [-4, 16],
+                "num3" => [10, 100],
+                "num4" => [0.2, 0.04],
+                "num5" => [-0.4, 0.16],
+                "num6" => [0.01, 0.0001],
+                "str1" => ['str', 'str'],
+                "str2" => ['2', '4'],
+                "str3" => ['@#$%', '@#$%'],
+                "obj1" => [$testObject, $testObject],
+                "arr1" => [$testArr, $testArr], 
             ];
         }
 
